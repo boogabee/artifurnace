445efeea209db82206c59439d6cb8c62e1bdbe98
Scott Kahler <scott.kahler@gmail.com>
2015-12-17 16:52:28 -0600

    Build in docker container working at this point. Working on deploy

--- centos6/build.sh
+++ centos6/build.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 set -e
-export WORKSPACE=/opt/build/
-yum -y install gcc wget tar git rpm-build ncurses-devel bzip2 bison flex openssl-devel libcurl-devel readline-devel bzip2-devel gcc-c++ libyaml-devel libevent-devel
+
+LAUNCH_DIR=`pwd`
+export WORKSPACE=/opt/gpdbbuild/
+yum -y install gcc wget tar git rpm-build ncurses-devel bzip2 bison flex openssl-devel libcurl-devel readline-devel bzip2-devel gcc-c++ libyaml-devel libevent-devel openldap-devel
 #yum -y install gcc wget tar git rpm-build ncurses-devel bzip2 bison flex apr-devel apr-util-devel openssl-devel libcurl-devel readline-devel bzip2-devel gcc-c++ libyaml-devel libevent-devel
 
-mkdir /opt/build/
-cd /opt/build/
+mkdir -p ${WORKSPACE}
+cd ${WORSPACE}
 
-git clone --depth=1 https://github.com/greenplum-db/gpdb.git /opt/build/
+git clone --depth=1 https://github.com/greenplum-db/gpdb.git ${WORKSPACE}
 
 export APR=apr-1.5.2
 export APR_UTIL=apr-util-1.5.4
@@ -151,20 +153,16 @@ tar -czvf /usr/local/${GPDB_PACKAGE_NAME}.tar.gz -C /usr/local ${GPDB_VERSION_NA
 
 #Build RPM
 cd ${WORKSPACE}
-cp /${WORKSPACE}/gpdb.spec ./SPECS/gpdb.spec
+cp ${LAUNCH_DIR}/gpdb.spec ${WORKSPACE}/SPECS/gpdb.spec
 cp /usr/local/${GPDB_PACKAGE_NAME}.tar.gz ./SOURCES/
 rpmbuild --define "gpdb_ver ${BUILD_DATE}" --define "gpdb_rel ${BUILD_NUMBER}" --define "_topdir "`pwd` -ba SPECS/gpdb.spec
 
-mkdir ${WORKSPACE}/output/
-cp /usr/local/${GPDB_PACKAGE_NAME}.tar.gz ${WORKSPACE}/output/${GPDB_PACKAGE_NAME}.tar.gz
+mkdir ${LAUNCH_DIR}/output/
+cp /usr/local/${GPDB_PACKAGE_NAME}.tar.gz ${LAUNCH_DIR}/output/${GPDB_PACKAGE_NAME}.tar.gz
 
-#for rpms in `ls -1 ${WORKSPACE}/RPMS/x86_64/`
-#do
-#  cp ${WORKSPACE}/RPMS/x86_64/${rpms} /opt/output/${rpms}
-#done
+for rpms in `ls -1 ${WORKSPACE}/RPMS/x86_64/`
+do
+  cp ${WORKSPACE}/RPMS/x86_64/${rpms} ${LAUNCH_DIR}/output/${rpms}
+done
 
-#for srpms in `ls -1 ${WORKSPACE}/SRPMS/`
-#do
-#  cp ${WORKSPACE}/SRPMS/${srpms} /opt/output/${srpms}
-#done
 
