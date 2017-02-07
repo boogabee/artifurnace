30d3cbbfbd9dcb454413187de3be361e7ef54c51
Akon Dey <akon@awakenetworks.com>
2017-02-06 23:02:27 -0800

    Enable Orca and ability to build from a commit SHA
    Fix indentation.

--- centos7/run_build_container.sh
+++ centos7/run_build_container.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash -x
 
 BUILD_DIR="/opt/build"
 
-docker run -v `pwd`:$BUILD_DIR -w $BUILD_DIR centos:7.1.1503 bash -c "${BUILD_DIR}/build.sh"
+docker run -v `pwd`:$BUILD_DIR -w $BUILD_DIR centos:7.2.1511 bash -c "${BUILD_DIR}/build.sh $*" | tee build.log 2>&1
